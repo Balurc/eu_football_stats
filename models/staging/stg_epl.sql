1a9c154f32ab645c3dc70f24c7bd2b79c6d97578
Balu-Rama-Chandra <baludbttutorial90@gmail.com>
2022-11-11 08:41:24 +0000

    commit staging

---
+++ models/staging/stg_epl.sql
@@ -0,0 +1,44 @@
+{{ config(materialized='view') }}
+
+with source_epl as (
+  select
+
+    -- ids
+    {{ dbt_utils.surrogate_key(['Div']) }} league_id,
+    {{ dbt_utils.surrogate_key(['season','HomeTeam','AwayTeam']) }} match_id,
+
+    -- dimensions
+    Div as league_name,
+    season as season,
+    HomeTeam as home_team,
+    AwayTeam as away_team,
+    FTR as full_time_result,
+    HTR as half_time_result,
+
+    -- measures
+    FTHG as full_time_home_goals,
+    FTAG as full_time_away_goals,
+    HTHG as half_time_home_goals,
+    HTAG as half_time_away_goals,
+    HS as home_shots,
+    `AS` as away_shots,
+    HST as home_shots_on_target,
+    AST as away_shots_on_target,
+    HC as home_corners,
+    AC as away_corners,
+    HF as home_fouls,
+    AF as away_fouls,
+    HY as home_yellow_cards,
+    AY as away_yellow_cards,
+    HR as home_red_cards,
+    AR as away_red_cards,
+
+    -- date/times
+    `Date` as old_formatted_date,
+    coalesce(safe.parse_date('%Y-%m-%d', Date), safe.parse_date('%d/%m/%y', Date), safe.parse_date('%d/%m/%Y', Date)) as new_formatted_date
+
+  from {{ source('staging','english-premier-league_external_table') }}
+  order by new_formatted_date
+)
+
+select * from source_epl
